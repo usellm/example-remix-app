f8fe70f7110374bbb557e480c56e31b0fdfed7e5
Siddhant <siddhant.ujjain@gmail.com>
2023-06-03 19:34:35 +0530

    Initial commit

---
+++ app/routes/_index.jsx
@@ -0,0 +1,187 @@
+/* 
+- Copy and paste this code into your Next.js applications's "app/page.tsx" file to get started 
+- Make sure to run "npm install usellm" to install the useLLM pacakge
+- Replace the `serviceUrl` below with your own service URL for production
+*/
+import { useEffect, useRef, useState } from "react";
+import useLLM from "usellm";
+
+export default function AIChatBot() {
+  const [status, setStatus] = useState("idle");
+  const [history, setHistory] = useState([
+    {
+      role: "assistant",
+      content:
+        "I'm a chatbot powered by the ChatGPT API and developed using useLLM. Ask me anything!",
+    },
+  ]);
+  const [inputText, setInputText] = useState("");
+
+  const llm = useLLM({
+    serviceUrl: "https://usellm.org/api/llm", // For testing only. Follow this guide to create your own service URL: https://usellm.org/docs/api-reference/create-llm-service
+  });
+
+  let messagesWindow = useRef(null);
+
+  useEffect(() => {
+    if (messagesWindow?.current) {
+      messagesWindow.current.scrollTop = messagesWindow.current.scrollHeight;
+    }
+  }, [history]);
+
+  async function handleSend() {
+    if (!inputText) {
+      return;
+    }
+    try {
+      setStatus("streaming");
+      const newHistory = [...history, { role: "user", content: inputText }];
+      setHistory(newHistory);
+      setInputText("");
+      const { message } = await llm.chat({
+        messages: newHistory,
+        stream: true,
+        onStream: ({ message }) => setHistory([...newHistory, message]),
+      });
+      setHistory([...newHistory, message]);
+      setStatus("idle");
+    } catch (error) {
+      console.error(error);
+      window.alert("Something went wrong! " + error.message);
+    }
+  }
+
+  async function handleRecordClick() {
+    try {
+      if (status === "idle") {
+        await llm.record();
+        setStatus("recording");
+      } else if (status === "recording") {
+        setStatus("transcribing");
+        const { audioUrl } = await llm.stopRecording();
+        const { text } = await llm.transcribe({ audioUrl });
+        setStatus("streaming");
+        const newHistory = [...history, { role: "user", content: text }];
+        setHistory(newHistory);
+        const { message } = await llm.chat({
+          messages: newHistory,
+          stream: true,
+          onStream: ({ message }) => setHistory([...newHistory, message]),
+        });
+        setHistory([...newHistory, message]);
+        setStatus("idle");
+      }
+    } catch (error) {
+      console.error(error);
+      window.alert("Something went wrong! " + error.message);
+    }
+  }
+
+  const Icon = status === "recording" ? Square : Mic;
+
+  return (
+    <div className="flex flex-col h-full max-h-[600px] overflow-y-hidden">
+      <div
+        className="w-full flex-1 overflow-y-auto px-4"
+        ref={(el) => (messagesWindow.current = el)}
+      >
+        {history.map((message, idx) => (
+          <Message {...message} key={idx} />
+        ))}
+      </div>
+      <div className="w-full pb-4 flex px-4">
+        <input
+          className="p-2 border rounded w-full block dark:bg-gray-900 dark:text-white"
+          type="text"
+          placeholder={getInputPlaceholder(status)}
+          value={inputText}
+          disabled={status !== "idle"}
+          onChange={(e) => setInputText(e.target.value)}
+          onKeyDown={(event) => {
+            if (event.key === "Enter" && !event.shiftKey) {
+              event.preventDefault();
+              handleSend();
+            }
+          }}
+        />
+        <button
+          className="p-2 border rounded bg-gray-100 hover:bg-gray-200 active:bg-gray-300 dark:bg-white dark:text-black font-medium ml-2"
+          onClick={handleSend}
+        >
+          Send
+        </button>
+        <button
+          className="p-2 border rounded bg-gray-100 hover:bg-gray-200 active:bg-gray-300 dark:bg-white dark:text-black font-medium ml-2"
+          onClick={handleRecordClick}
+        >
+          <Icon />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+const Mic = () => (
+  // you can also use an icon library like `react-icons` here
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z"></path>
+    <path d="M19 10v2a7 7 0 0 1-14 0v-2"></path>
+    <line x1="12" x2="12" y1="19" y2="22"></line>
+  </svg>
+);
+
+const Square = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <rect width="18" height="18" x="3" y="3" rx="2" ry="2"></rect>
+  </svg>
+);
+
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.substring(1);
+}
+
+function getInputPlaceholder(status) {
+  switch (status) {
+    case "idle":
+      return "Ask me anthing...";
+    case "recording":
+      return "Recording audio...";
+    case "transcribing":
+      return "Transcribing audio...";
+    case "streaming":
+      return "Wait for my response...";
+  }
+}
+
+function Message({ role, content }) {
+  return (
+    <div className="my-4">
+      <div className="font-semibold text-gray-800 dark:text-white">
+        {capitalize(role)}
+      </div>
+      <div className="text-gray-600 dark:text-gray-200 whitespace-pre-wrap mt-1">
+        {content}
+      </div>
+    </div>
+  );
+}
